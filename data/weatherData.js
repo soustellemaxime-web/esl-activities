36ddd7f03627262d29ecc762a5b4b433814dd59f
Maxime Soustelle <soustelle.maxime@gmail.com>
2026-01-31 09:09:07 +0700

    fixed paths

--- data/weatherData.js
+++ data/weatherData.js
@@ -1,11 +1,11 @@
 export const weatherData = [
-  { name: "Sunny", id: "sunny", img: "../assets/images/weather/sunny.png" },
-  { name: "Rainy", id: "rainy", img: "../assets/images/weather/rainy.png" },
-  { name: "Snowy", id: "snowy", img: "../assets/images/weather/snowy.png" }
+  { name: "Sunny", id: "sunny", img: "assets/images/weather/sunny.png" },
+  { name: "Rainy", id: "rainy", img: "assets/images/weather/rainy.png" },
+  { name: "Snowy", id: "snowy", img: "assets/images/weather/snowy.png" }
 ];
 
 export const clothesData = [
-  { name: "T-shirt", target: "sunny", img: "../assets/images/clothes/shirt.png" },
-  { name: "Umbrella", target: "rainy", img: "../assets/images/clothes/umbrella.png" },
-  { name: "Jacket", target: "snowy", img: "../assets/images/clothes/jacket.png" }
+  { name: "T-shirt", target: "sunny", img: "assets/images/clothes/shirt.png" },
+  { name: "Umbrella", target: "rainy", img: "assets/images/clothes/umbrella.png" },
+  { name: "Jacket", target: "snowy", img: "assets/images/clothes/jacket.png" }
 ];
